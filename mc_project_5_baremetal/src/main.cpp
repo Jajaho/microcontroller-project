575540524d0ee79261befce7730c41574fd739d2
Jakob <jajaho2@web.de>
2023-05-22 14:52:00 +0200

    Adds bool GPIO_get_Input()

--- mc_project_5_baremetal/src/main.cpp
+++ mc_project_5_baremetal/src/main.cpp
@@ -1,4 +1,7 @@
-#include  <Arduino.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <Adafruit_TinyUSB.h>
+
 // Definitions, cf. Datasheet
 #define P0_BASE_ADDRESS 0x50000000
 #define P1_BASE_ADDRESS 0x50000300
@@ -6,6 +9,13 @@
 #define DIRCLR_OFFSET 0x51C
 #define OUTSET_OFFSET 0x508
 #define OUTCLR_OFFSET 0x50C
+#define IN 0x510 
+// MPRS Conversion Constants
+#define OUT_MAX 15099494    // (90% of 224 counts or 0xE66666)
+#define OUT_MIN  1677722    // (10% of 224 counts or 0x19999A) 
+#define P_MAX 25
+#define P_MIN 0
+
 // Make a PIN an output
 void GPIO_set_to_Output(uint8_t port, uint8_t pin_number) {
     uint32_t * tmp = (uint32_t*) ((port==1?P1_BASE_ADDRESS:P0_BASE_ADDRESS) + DIRSET_OFFSET);
@@ -22,8 +32,13 @@ void GPIO_set_to_Input(uint8_t port, uint8_t pin_number) {
     *tmp = (1<<pin_number);
 }
 
+bool GPIO_get_Input(uint8_t port, uint8_t pin_number) {
+    uint32_t * tmp = (uint32_t*) ((port==1?P1_BASE_ADDRESS:P0_BASE_ADDRESS) + IN);
+    return (1<<pin_number) & *tmp;
+}
+
 double MPRLS_binary_to_pressure(uint32_t binary) {
-    
+    return ((binary - OUT_MIN)*(P_MAX - P_MIN))/(OUT_MAX - OUT_MIN) + P_MIN;
 }
 
 // Arduino IDE: Initialization
