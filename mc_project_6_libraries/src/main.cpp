9042bdf6f4c98963375e988c55d5fc4d25d0a54a
Jakob <jajaho2@web.de>
2023-06-05 16:29:48 +0200

    Adds display hello world (mc_project_6 task 4)

--- mc_project_6_libraries/src/main.cpp
+++ mc_project_6_libraries/src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
 #include <Adafruit_TinyUSB.h>
-#include "Adafruit_MPRLS.h"
+#include <Adafruit_MPRLS.h>
 
 #define RESET_PIN -1 // set to any GPIO pin # to hard-reset on begin()
 #define EOC_PIN -1 // set to any GPIO pin to read end-of-conversion by pin
@@ -14,6 +16,14 @@
 
 #define VALVE A0
 #define PUMP A1
+
+// Waveshare 17344
+#define TFT_CS A5   //chip select *put your pin here*
+#define TFT_DC A2    // data/command *put your pin here*
+#define TFT_RST A4   // reset *put your pin here*
+#define TFT_BL A3    // backlight *put your pin here*
+//input: chip select; data/command and reset pin -> reqquired for the library
+Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 Adafruit_MPRLS pressureSensor = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
@@ -27,6 +37,9 @@ unsigned long startTimer = 0;     // Timestamp of the last measurement
 unsigned long sensorSpeed = 0;    // Time it took the sensor to measure the pressure and communicate to the uC
 unsigned long measPeriod = 300;   // Set the measurement interval to 10ms
 unsigned long timeStamp = 0;      // Set at the beginning to achieve constant measurement period
+
+int16_t xCursor = 0;
+int16_t yCursor = 0;
 
 void panicISR();
 void pumpControlISR();
@@ -47,7 +60,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);   
   digitalWrite(LED_BUILTIN, LOW);
 
-  //Actors:
+  //Actuators:
   pinMode(VALVE, OUTPUT);
   digitalWrite(VALVE, HIGH);
 
@@ -58,14 +71,28 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BTN_GREEN), pumpControlISR, FALLING);
 
   Serial.begin(115200);
-  Serial.println("MPRLS Simple Test");
+  Serial.println("Initializing MPRLS");
   if (! pressureSensor.begin()) {
     Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("Found MPRLS sensor");
+  Serial.println("MPRLS sensor initialized.");
+
+  pinMode(TFT_BL, OUTPUT);
+  pinMode(TFT_CS, OUTPUT);
+  pinMode(TFT_RST, OUTPUT);
+  //initializing of display
+  display.init(240, 320);
+  display.setRotation(3);
+  display.fillScreen(ST77XX_BLACK);
+  display.setCursor(0, 0);
+  display.setTextWrap(true);
+  display.setTextSize(4);
+
+  display.print("Hello World!");
+
 }
 
 void loop() {
