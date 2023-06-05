a77176cb73859a2b8f2a937b33c284d2cce7608a
Jakob <jajaho2@web.de>
2023-06-05 15:59:03 +0200

    task project_6 3.2 done

--- mc_project_6_libraries/src/main.cpp
+++ mc_project_6_libraries/src/main.cpp
@@ -12,38 +12,50 @@
 #define BTN_RED 9
 #define BTN_GREEN 7
 
+#define VALVE A0
+#define PUMP A1
+
 Adafruit_MPRLS pressureSensor = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+
 float pressure_hPa = 0;
-int count = 0; //number of seconds
+float targetPressure = 1000;
+
 volatile bool panicFlag = false;  //flag to signal that an interrupt has been detected
+volatile bool pumpFlag = false;
+
 unsigned long startTimer = 0;     // Timestamp of the last measurement
-unsigned long endTimer = 0;       // 
 unsigned long sensorSpeed = 0;    // Time it took the sensor to measure the pressure and communicate to the uC
-unsigned long measPeriod = 100;   // Set the measurement interval to 10ms
-unsigned long realPeriod = 0;
+unsigned long measPeriod = 300;   // Set the measurement interval to 10ms
 unsigned long timeStamp = 0;      // Set at the beginning to achieve constant measurement period
 
-void interruptFunction();
+void panicISR();
+void pumpControlISR();
 
 void setup() {
-  // Inputs
-  pinMode(BTN_RED, INPUT_PULLUP);
-  pinMode(BTN_GREEN, INPUT_PULLUP);
+  // -----  Inputs  --------
+  pinMode(BTN_RED, INPUT_PULLUP);   // Button is active low!
+  pinMode(BTN_GREEN, INPUT_PULLUP); // Button is active low!
 
-  // Outputs
+  // -----  Outputs  --------
+  // LEDs:
   pinMode(LED_GREEN, OUTPUT);
   digitalWrite(LED_GREEN, LOW);
-  
+
   pinMode(LED_RED, OUTPUT);
   digitalWrite(LED_RED, LOW);
 
   pinMode(LED_BUILTIN, OUTPUT);   
   digitalWrite(LED_BUILTIN, LOW);
 
-  //pinMode(PIN_BUTTON1, INPUT_PULLUP);   //Declare the pin as interrupt, interrupts can be detected also by RISING, FALLING and level
-  attachInterrupt(digitalPinToInterrupt(BTN_RED), interruptFunction, CHANGE);
-  
-  digitalWrite(LED_BUILTIN, LOW);
+  //Actors:
+  pinMode(VALVE, OUTPUT);
+  digitalWrite(VALVE, HIGH);
+
+  pinMode(PUMP, OUTPUT);   
+  digitalWrite(PUMP, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(BTN_RED), panicISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BTN_GREEN), pumpControlISR, FALLING);
 
   Serial.begin(115200);
   Serial.println("MPRLS Simple Test");
@@ -61,13 +73,37 @@ void loop() {
   if(panicFlag) {
     Serial.println("Panic! Operation has been halted. Reset to continue.");
     digitalWrite(LED_RED, HIGH);
-    delay(1000);
-    return;
+    digitalWrite(PUMP, LOW);
+    digitalWrite(BTN_GREEN, LOW);
+  }
+  else {
+    digitalWrite(LED_RED, LOW);
   }
 
-  timeStamp = millis();
+  if(pumpFlag)
+  {
+    digitalWrite(PUMP, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+  }
+  else {
+    digitalWrite(PUMP, LOW);
+    digitalWrite(LED_GREEN, LOW);
+  }
+  
+  if (pumpFlag && (pressure_hPa >= targetPressure))
+  {
+    pumpFlag = false;
+  }
+  
+  
 
+  timeStamp = millis();
   if (timeStamp - startTimer >= measPeriod) {
+    if (pumpFlag) {
+      digitalWrite(VALVE, LOW);
+      delay(100);
+    }
+    
     startTimer = timeStamp;
 
     pressure_hPa = pressureSensor.readPressure();
@@ -77,19 +113,27 @@ void loop() {
     Serial.println(pressure_hPa);
     Serial.print("Read-out speed (ms): "); //Print the read-out time of one measurement
     Serial.println(sensorSpeed);
-    Serial.print("Measurement Interval: "); //Print your set measurement interval
-    Serial.println(realPeriod);
 
+    /*
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
+    */  
+    Serial.print("panicFlag: ");
+    Serial.println(panicFlag);
+    Serial.print("pumpFlag: ");
+    Serial.println(pumpFlag);
   }
-  /* Put your code here */
-  //Serial.print("Time (s): ");
-  //Serial.println(count);
+  
 }
 
-void interruptFunction(){
+void panicISR() {
   panicFlag = true;
+  pumpFlag = false;
+}
+
+void pumpControlISR() {
+  pumpFlag = true;
+  panicFlag = false;
 }
