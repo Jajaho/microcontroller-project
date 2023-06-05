02102ccf745812080489e5f3f7c827e1f41cf63d
Jakob <jajaho2@web.de>
2023-06-05 14:32:09 +0200

    Adds panic functionality with big red button

--- mc_project_6_libraries/src/main.cpp
+++ mc_project_6_libraries/src/main.cpp
@@ -7,13 +7,15 @@
 #define RESET_PIN -1 // set to any GPIO pin # to hard-reset on begin()
 #define EOC_PIN -1 // set to any GPIO pin to read end-of-conversion by pin
 
-#define LED 3
-#define SWITCH 4
+#define LED_RED 5
+#define LED_GREEN 10
+#define BTN_RED 9
+#define BTN_GREEN 7
 
 Adafruit_MPRLS pressureSensor = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 float pressure_hPa = 0;
 int count = 0; //number of seconds
-volatile bool flagStart = false;  //flag to signal that an interrupt has been detected
+volatile bool panicFlag = false;  //flag to signal that an interrupt has been detected
 unsigned long startTimer = 0;     // Timestamp of the last measurement
 unsigned long endTimer = 0;       // 
 unsigned long sensorSpeed = 0;    // Time it took the sensor to measure the pressure and communicate to the uC
@@ -21,11 +23,26 @@ unsigned long measPeriod = 100;   // Set the measurement interval to 10ms
 unsigned long realPeriod = 0;
 unsigned long timeStamp = 0;      // Set at the beginning to achieve constant measurement period
 
-void setup() {
+void interruptFunction();
 
-  pinMode(PIN_BUTTON1, INPUT_PULLUP);   //Declare the pin as interrupt, interrupts can be detected also by RISING, FALLING and level
-  attachInterrupt(digitalPinToInterrupt(SWITCH), interruptFunction, CHANGE);
-  pinMode(LED_BUILTIN, OUTPUT);         //Turn the LED on if the button is pressed
+void setup() {
+  // Inputs
+  pinMode(BTN_RED, INPUT_PULLUP);
+  pinMode(BTN_GREEN, INPUT_PULLUP);
+
+  // Outputs
+  pinMode(LED_GREEN, OUTPUT);
+  digitalWrite(LED_GREEN, LOW);
+  
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_RED, LOW);
+
+  pinMode(LED_BUILTIN, OUTPUT);   
+  digitalWrite(LED_BUILTIN, LOW);
+
+  //pinMode(PIN_BUTTON1, INPUT_PULLUP);   //Declare the pin as interrupt, interrupts can be detected also by RISING, FALLING and level
+  attachInterrupt(digitalPinToInterrupt(BTN_RED), interruptFunction, CHANGE);
+  
   digitalWrite(LED_BUILTIN, LOW);
 
   Serial.begin(115200);
@@ -40,6 +57,14 @@ void setup() {
 }
 
 void loop() {
+
+  if(panicFlag) {
+    Serial.println("Panic! Operation has been halted. Reset to continue.");
+    digitalWrite(LED_RED, HIGH);
+    delay(1000);
+    return;
+  }
+
   timeStamp = millis();
 
   if (timeStamp - startTimer >= measPeriod) {
@@ -61,10 +86,10 @@ void loop() {
     delay(100);
   }
   /* Put your code here */
-  Serial.print("Time (s): ");
-  Serial.println(count);
+  //Serial.print("Time (s): ");
+  //Serial.println(count);
 }
 
 void interruptFunction(){
-/* Put your code here */
+  panicFlag = true;
 }
